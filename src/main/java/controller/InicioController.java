7c0d59b3c43484072b63e46afc78507a93e01b80
angieVa <angievalentina1526@gmail.com>
2018-08-23 18:52:31 -0500

    interface connected to model, listview show the numbers

--- src/main/java/controller/InicioController.java
+++ src/main/java/controller/InicioController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,12 +11,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import model.Sort;
 
 public class InicioController {
 		
 	
 
-		@FXML
+	 @FXML
 	    private Button butOrder;
 
 	    @FXML
@@ -30,6 +33,9 @@ public class InicioController {
 	    private TextField txtIntervalU;
 
 	    @FXML
+	    private RadioButton rbNoRepeat;
+
+	    @FXML
 	    private ToggleGroup type;
 
 	    @FXML
@@ -39,7 +45,10 @@ public class InicioController {
 	    private TextField txtPercentage;
 
 	    @FXML
-	    private ListView<?> listValues;
+	    private ListView<Double> listValues= new ListView<Double>();
+
+	    @FXML
+	    private RadioButton rbRepeat;
 
 	    @FXML
 	    private RadioButton rbRandomOrder;
@@ -49,6 +58,9 @@ public class InicioController {
 
 	    @FXML
 	    private Button butGenerate;
+
+	    @FXML
+	    private ToggleGroup repeat;
 
 	    @FXML
 	    private TextField txtValues;
@@ -84,7 +96,53 @@ public class InicioController {
 
 	    @FXML
 	    void GenerateNumbers(ActionEvent event) {
-
+	    	int option = 0;
+	    	int optionRandom = 0;
+	    	double rank1 = 0;
+	    	double rank2 = 0;
+	    	double percentage = 0;
+	    	int quantity = 0;
+	    	
+	    	if(rbRepeat.isSelected()==true){
+	    		
+	    		option = 0;
+	    		
+	    		
+	    	}else if(rbNoRepeat.isSelected()== true) {
+	    		option = 1;
+	    			
+	    	}
+	    	
+	    	rank1 = Double.parseDouble(txtIntervalL.getText());
+	    	rank2 = Double.parseDouble(txtIntervalU.getText());
+	    	percentage = Double.parseDouble(txtPercentage.getText())/100;
+	    	quantity = Integer.parseInt(txtData.getText());
+	    	
+	    	if(rbRandomOrder.isSelected() == true) {
+	    		optionRandom = 1;
+	    		
+	    	}else if(rbOrderedValues.isSelected() == true) {
+	    		optionRandom = 2;
+	    		
+	    	}else if(rbReverseOrder.isSelected() == true) {
+	    		optionRandom = 3;
+	    		
+	    	}else if(rbPercentage.isSelected() == true) {
+	    		optionRandom = 4;
+	    		
+	    	}
+	    	
+	    	Sort sort = new Sort( quantity, rank1, rank2, option, optionRandom, percentage);
+	    	
+	    	ObservableList<Double> items =FXCollections.observableArrayList(); 
+	   
+	    	for(int i = 0; i < quantity; i++) {
+	    		items.add(sort.getArray()[i]);
+	    		
+	    	}
+	    	
+	    	listValues.setItems(items);
+	    	
+	    	
 	    }
-
 }
