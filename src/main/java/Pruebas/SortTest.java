337db19b0846dea1871778edeed6a88d4092f94d
JAir Aguirre <aguirre.jair@gmail.com>
2018-08-23 23:40:16 -0500

    Package Test added

---
+++ src/main/java/Pruebas/SortTest.java
@@ -0,0 +1,19 @@
+package Pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class SortTest {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
